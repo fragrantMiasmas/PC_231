6e7545be022a367ef4959970c4f3112feb5eb70a
fragrantMiasmas <exreed@willamette.edu>
2017-04-09 23:16:20 -0700

    PC_231

---
+++ src/PC_231/CPU.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PC_231;
+
+/**
+ *
+ * @author ElizabethReed PC
+ */
+public class CPU {
+    
+    public int[] R0(){
+        int[] R0 = new int[12];
+        return R0;
+    }
+    
+     public int[] R1(){
+        int[] R1 = new int[12];
+        return R1;
+    }
+    
+    public int[] R2(){
+        int[] R1 = new int[12];
+        return R1;
+    }
+    
+     public int[] R3(){
+        int[] R3 = new int[12];
+        return R3;
+    }
+    
+    public int[] R4(){
+        int[] R4 = new int[12];
+        return R4;
+    }
+    
+    public int[] R5(){
+        int[] R5 = new int[12];
+        return R5;
+    }
+    
+    public int[] R6(){
+        int[] R6 = new int[12];
+        return R6;
+    }
+    
+    public int[] R7(){
+        int[] R7 = new int[12];
+        return R7;
+    }
+    
+    public int[] R8(){
+        int[] R8 = new int[12];
+        return R8;
+    }
+    
+     public int[] R9(){
+        int[] R9 = new int[12];
+        return R9;
+    }
+     
+    public int[] J0(){
+        int[] J0 = new int[12];
+        return J0;
+    }
+    
+    public int[] J1(){
+        int[] J1 = new int[12];
+        return J1;
+    }
+   
+    public int[] J2(){
+        int[] J2 = new int[12];
+        return J2;
+    }
+    
+    public int[] J3(){
+        int[] J3 = new int[12];
+        return J3;
+    }
+    
+    public int[] DR(){
+        int[] DR = new int[12];
+        return DR;
+    }
+    
+    public int[] PC(){
+        int[] PC = new int[12];
+        return PC;
+    }
+}
---
+++ src/PC_231/Sim231.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PC_231;
+
+/**
+ *
+ * @author ElizabethReed PC
+ */
+public class Sim231 {
+    
+   
+    
+}
