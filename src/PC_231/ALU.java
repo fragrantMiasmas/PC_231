76cdb78713caf513ff0f64e514a83c6d613cdf7d
fragrantMiasmas <exreed@willamette.edu>
2017-04-09 23:30:16 -0700

    ALU initialized

---
+++ src/PC_231/ALU.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PC_231;
+
+/**
+ *
+ * @author ElizabethReed PC
+ */
+public class ALU {
+    
+    public void Halt(){
+        
+    }
+    
+    public void zero(){
+        
+    }
+    
+    public void set(){
+        
+    }
+    
+    public void data(){
+        
+    }
+    
+    public void INC(){
+        
+    }
+    
+    public void Shift(){
+        
+    }
+    
+    public void add(){
+        
+    }
+    
+    public void sub(){
+        
+    }
+    
+    public void and(){
+        
+    }
+    
+    public void copy(){
+        
+    }
+    
+    public void load(){
+        
+    }
+    
+    public void store(){
+        
+    }
+    
+    public void read(){
+        
+    }
+    
+    public void write(){
+        
+    }
+    
+    public void JPIF(){
+        
+    }
+    
+    public void jump(){
+        
+    }
+}
