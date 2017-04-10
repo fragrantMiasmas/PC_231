73b4f9d84a79bab7ce4ad6b9e379d3ed42417ea4
fragrantMiasmas <exreed@willamette.edu>
2017-04-09 23:47:13 -0700

    Comments added

--- src/PC_231/ALU.java
+++ src/PC_231/ALU.java
@@ -10,69 +10,84 @@ package PC_231;
  * @author ElizabethReed PC
  */
 public class ALU {
+
     
-    //test test
-    
+    //Halts the machine
     public void Halt(){
         
     }
     
+    //	Zeroes (or "clears") out register RRRR
     public void zero(){
         
     }
     
+    //	Sets the 4 lowest-order bits of register RRRR to BBBB
     public void set(){
         
     }
     
+    //	Clears the data register DR, then sets its 8 lowest-order bits
     public void data(){
         
     }
     
+    //	Adds or subtracts from 1 to 8 from register RRRR
     public void INC(){
         
     }
     
+    //Shifts register RRRR left (-) or right (+) by from 1 to 8 bits
     public void Shift(){
         
     }
     
+    //Adds the contents of register RRR1 to RRR2 (result in RRR2)
     public void add(){
         
     }
     
+    //	Subtracts the contents of register RRR1 from RRR2 (result in 
     public void sub(){
         
     }
     
+    //	Logically ANDs the contents of register RRR1 into RRR2 (result in RRR2)
     public void and(){
         
     }
     
+    //Copies the contents of register RRR1 to RRR2
     public void copy(){
         
     }
     
+    //	Loads the contents of location addressed by RRR2 into register RRR1
     public void load(){
         
     }
     
+    //  Stores the contents of register RRR1 into location addressed by RRR2
     public void store(){
         
     }
     
+    //  Reads a value (up to 12 bits) into register RRRR from device DDDD
     public void read(){
         
     }
     
+    //  Writes a value (up to 12 bits) from register RRRR to device DDDD
     public void write(){
         
     }
     
+    //  If contents of RRRR meet condition CC, jump to address in JJ
     public void JPIF(){
         
     }
     
+    //  Jump directly to the location whose address is AAAA AAAA
     public void jump(){
         
     }
